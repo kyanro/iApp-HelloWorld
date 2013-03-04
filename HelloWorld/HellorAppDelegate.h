603d5a2ca4d36e58a23d8aa9440accda0cb4aeae
kyanro <gw.fnct+github-pub@gmail.com>
2013-03-04 22:02:28 +0900

    Initial Commit

---
+++ HelloWorld/HellorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HellorAppDelegate.h
+//  HelloWorld
+//
+//  Created by ppp on 2013/03/04.
+//  Copyright (c) 2013年 ppp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HellorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by ppp on 2013/03/04.
+//  Copyright (c) 2013年 ppp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HellorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HellorAppDelegate class]));
+    }
+}
